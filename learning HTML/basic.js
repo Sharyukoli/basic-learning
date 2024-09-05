f6a460b3c83b14cba5e56d7a68aa611c2e1d938c
Sharyukoli <kolisharyu@gmail.com>
2024-09-05 15:47:42 +0530

    files added

---
+++ learning HTML/basic.js
@@ -0,0 +1,11 @@
+var a=5
+let b =10
+var result =a*b
+
+function basicjs()
+            {
+                var result=a*b;
+                console.log(result);
+                alert("The result of Addition is: " + result);
+
+            }
