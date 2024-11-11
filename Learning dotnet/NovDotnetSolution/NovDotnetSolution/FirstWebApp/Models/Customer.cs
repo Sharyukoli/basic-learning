d93a8318a443ab0826c1b4128bb94d5fab5a7e58
Sharyukoli <kolisharyu@gmail.com>
2024-11-11 11:41:34 +0530

    changes added

---
+++ Learning dotnet/NovDotnetSolution/NovDotnetSolution/FirstWebApp/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstWebApp.Models
+{
+    public class Customer
+    {
+        public string Name {  get; set; }
+        public string Email { get; set; }
+        public string ContactNumber {  get; set; }
+    }
+}
---
+++ Learning dotnet/NovDotnetSolution/NovDotnetSolution/FirstWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstWebApp.Pages
+{
+    public class IndexModel : PageModel
+
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public  List<string> Technologies { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Technologies = new List<string>();
+            Technologies.Add("Java");
+            Technologies.Add("C#");
+            Technologies.Add("C++");
+            Technologies.Add("Javascript");
+        }
+    }
+}
