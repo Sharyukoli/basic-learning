0fc99e50616ceee9c4920ac842f711972b003e51
Sharyukoli <kolisharyu@gmail.com>
2024-11-14 14:58:25 +0530

    changes added

--- Learning Python/days/day8/eighthproject/task/forms.py
+++ Learning Python/days/day8/eighthproject/task/forms.py
@@ -4,4 +4,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description']
+        fields = ['title',  'description']
+
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 2, 'cols': 40}),  # Adjust rows and columns as needed
+        }
