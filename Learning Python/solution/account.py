42b4b7b703830e82c4333943de5a8daa222b1b67
Sharyukoli <kolisharyu@gmail.com>
2024-11-14 15:02:50 +0530

    changes added

--- Learning Python/solution/account.py
+++ Learning Python/solution/account.py
@@ -1,13 +1,18 @@
-class  Account:
+class Account:
     def __init__(self, balance):
-        self._balance = balance  # Protected attribute
+        self._balance = balance  # Initialize the protected attribute _balance with the initial balance provided
 
     def deposit(self, amount):
-        self._balance += amount
+        self._balance += amount  # Add the specified amount to _balance, increasing the account balance
 
-    def get_balance(self):
-        return self._balance
+    def get_balance(self):  # Define a method to retrieve the current account balance
+        return self._balance  # Return the current value of the protected attribute _balance
 
-account =  Account(100)
+# Create an instance of the Account class with an initial balance of 100
+account = Account(100)
+
+# Call the deposit method to add 50 to the account balance
 account.deposit(50)
+
+# Print the current balance using the get_balance method
 print(account.get_balance())  # Output: 150
--- Learning Python/solution/address.py
+++ Learning Python/solution/address.py
@@ -1,8 +1,8 @@
-marks=89
-result=marks
-score=88
+marks = 89  # Assign 89 to the variable marks
+result = marks  # Assign the value of marks to result (result now references the same integer object as marks)
+score = 88  # Assign 88 to the variable score
 
-print(marks)
-print(id(marks))
-print(id(result))
-print(id(score))
+print(marks)  # Print the value of marks (expected output: 89)
+print(id(marks))  # Print the memory address (ID) of the object referenced by marks
+print(id(result))  # Print the memory address (ID) of the object referenced by result (should be the same as marks)
+print(id(score))  # Print the memory address (ID) of the object referenced by score (different from marks and result)
