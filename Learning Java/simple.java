adf0019611f5a12254593cc6d21f158de51241df
Sharyukoli <kolisharyu@gmail.com>
2024-11-18 17:32:05 +0530

    changes added

---
+++ Learning Java/simple.java
@@ -0,0 +1,12 @@
+public class simple {
+
+    public static void main(String[] args) {
+
+        String s = "sharyukoli";
+        String b = "rutujatambade";
+
+        System.out.println(s.concat(b));
+
+    }
+
+}
