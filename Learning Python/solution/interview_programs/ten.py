9966b5982590cfc6de1aa2c2906e57695b925e17
Sharyukoli <kolisharyu@gmail.com>
2024-11-18 17:54:21 +0530

    changes added

---
+++ Learning Python/solution/interview_programs/ten.py
@@ -0,0 +1,16 @@
+# Program to check if a number is a prime number
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Input from the user55
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
