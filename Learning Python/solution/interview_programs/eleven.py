c6796caa652b16aa843b72677ce1167dec0917f6
Sharyukoli <kolisharyu@gmail.com>
2024-11-19 18:19:41 +0530

    changes added

---
+++ Learning Python/solution/interview_programs/eleven.py
@@ -0,0 +1,22 @@
+#Question : Write a Python program to print "Hello, World!".
+# Explanation
+print("Hello, World!")
+
+#Question : Calculate the sum of two numbers entered by the user.
+# Explanation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+print("Sum:", sum_result)
+
+#Question : Write a Python program to check if a given number is even or odd.
+# Explanation
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+  print("Even")
+else:
+  print("Odd")
+
+  
+
+
