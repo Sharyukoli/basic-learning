d6106ccead44fa9563d63b9aee4f4fcd227cb703
Sharyukoli <kolisharyu@gmail.com>
2024-11-19 18:22:02 +0530

    changes added

--- Learning Python/solution/interview_programs/eleven.py
+++ Learning Python/solution/interview_programs/eleven.py
@@ -17,6 +17,29 @@ if num % 2 == 0:
 else:
   print("Odd")
 
+  #Question : Calculate the factorial of a given number.
+# Explanation
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial(num))
+
+#Question : Find the largest among three numbers entered by the user.
+# Explanation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest = max(num1, num2, num3)
+print("Largest number:", largest)
+
+#Question : Write a Python program to print all even numbers from 1 to 20.
+# Explanation
+for i in range(2, 21, 2):
+  print(i)
+
   
-
-
