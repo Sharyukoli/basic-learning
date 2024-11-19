4546b4c4205ea5c42733a4289ab8c78b42528587
Sharyukoli <kolisharyu@gmail.com>
2024-11-19 18:39:50 +0530

    changes added

--- Learning Python/solution/interview_programs/twelth.py
+++ Learning Python/solution/interview_programs/twelth.py
@@ -1,1 +1,23 @@
 #####
+#Write a program to find GCD of two numbers.
+#Here is the Program to find GCD of two numbers:
+def gcd(a, b):
+
+	if (a == 0):
+		return b
+	if (b == 0):
+		return a
+
+	if (a == b):
+		return a
+
+	if (a > b):
+		return gcd(a-b, b)
+	return gcd(a, b-a)
+
+a = 98
+b = 56
+if(gcd(a, b)):
+	print('GCD of', a, 'and', b, 'is', gcd(a, b))
+else:
+	print('not found')
