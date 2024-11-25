98275df06020b9ce5fb6985bc118ddccf54ce02c
Sharyukoli <kolisharyu@gmail.com>
2024-11-25 16:19:56 +0530

    changes added

--- Learning Python/days/day10/task_scheduler/scheduler/urls.py
+++ Learning Python/days/day10/task_scheduler/scheduler/urls.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('tasks/', views.task_list, name='task_list'),  # Task list page
-    path('create/', views.create_task, name='create_task'),  # Create task page
+    path('create/', views.create_task, name='create_task'), 
+      path('login/', LoginView.as_view(template_name='scheduler/login.html'), name='login'),
+    # Create task page
    
 ]
+
+
+from django.contrib.auth.views import LoginView
